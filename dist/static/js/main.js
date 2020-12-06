43f5fd525d8bab9b7316d7c88cb1162d1eba20d3
Mher-777 <mherjan2003@mail.ru>
2020-12-06 19:20:44 +0400

    start project and add header

---
+++ dist/static/js/main.js
@@ -0,0 +1,33 @@
+"use strict";
+
+function canUseWebp() {
+  var elem = document.createElement('canvas');
+
+  if (!!(elem.getContext && elem.getContext('2d'))) {
+    return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
+  }
+
+  return false;
+}
+
+window.onload = function () {
+  var images = document.querySelectorAll('[data-bg]');
+
+  for (var i = 0; i < images.length; i++) {
+    var image = images[i].getAttribute('data-bg');
+    images[i].style.backgroundImage = 'url(' + image + ')';
+  }
+
+  var isitFirefox = window.navigator.userAgent.match(/Firefox\/([0-9]+)\./);
+  var firefoxVer = isitFirefox ? parseInt(isitFirefox[1]) : 0;
+
+  if (canUseWebp() || firefoxVer >= 65) {
+    var imagesWebp = document.querySelectorAll('[data-bg-webp]');
+
+    for (var _i = 0; _i < imagesWebp.length; _i++) {
+      var imageWebp = imagesWebp[_i].getAttribute('data-bg-webp');
+
+      imagesWebp[_i].style.backgroundImage = 'url(' + imageWebp + ')';
+    }
+  }
+};
